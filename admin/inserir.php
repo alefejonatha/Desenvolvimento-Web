62804c8aadc6ffd972dd0ad7f10fd5da6e9eb478
alefejonatha <60615421+alefejonatha@users.noreply.github.com>
2020-06-04 09:15:07 -0300

    Update inserir.php

--- admin/inserir.php
+++ admin/inserir.php
@@ -1,6 +1,6 @@
 <H1>Inserir</H1>
 
-<form id="contactForm" action="?pg=inserirBanco" method="post">
+<form id="contactForm" action="?pg=inserirBanco" method="POST" enctype="multipart/form-data">
     <label>Titulo do Card: </label>
         <input class="form-control" name="tituloCard" type="text"/>
         
@@ -8,8 +8,9 @@
     <textarea class="form-control" name="textoCard" id="texto"></textarea>
 
     <label>Foto do Card: </label>
-       <textarea class="form-control" name="linkFotoCard" id="texto"></textarea>
-
+      <input type="file" name="assets" class="m-3">
+      
+      <br>
     <label>Titulo do Artigo: </label>
        <input class="form-control" name="tituloArt" type="text"/>
 
@@ -18,9 +19,6 @@
 
     <label>Link do video: </label>
        <textarea class="form-control" name="linkVideoArt" id="texto"></textarea>
-
     <hr>
-    <button class="btn btn-primary" name="Enviar">Cadastrar</button>
+    <button class="btn btn-primary" name="Enviar" type="submit">Cadastrar</button>
 </form>
-
-
